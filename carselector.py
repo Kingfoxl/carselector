4b3cde53910c782f2516812453b3ff23feacc3d8
Thomas Kingsrød <Thomas.kingsrod@gmail.com>
2024-11-04 21:58:55 +0100

    Add files via upload

---
+++ carselector.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov  4 20:30:39 2024
+
+@author: tking
+"""
+#Elbil = 4000 i strøm, 3058 i trafikkforsikringsavgift, 1000 i bom, 5000 i forsikring
+#Bensinbil = 10000 i bensin, 3058 i trafikkforsikringsavgift, 3000 i bom, 7500 i forsikring
+E = 4000+3058+1000+5000 #årlige kostnadene ved elbil
+B = 10000+3058+3000+7500 #årlige kostnadene ved bensinbil
+print("Bensinbil koster", B-E, "kroner mer enn Elbil per år.")
+
